493eda6440544bc5fec5eb765d09228a6759df3f
anirudh sai <anirudh.sai.581@gmail.com>
2025-08-14 00:33:54 +0530

    remove duplicates from array brute force code added

--- Basic/Array/RemoveDuplicatesFromArray.js
+++ Basic/Array/RemoveDuplicatesFromArray.js
@@ -13,6 +13,33 @@ Constraints:
 1 <= nums.length <= 105
 -104 <= nums[i] <= 104
 nums is sorted in non-decreasing order.
+
+Bruteforce: To use set data structure ,insert all elements into set , it will only accept unique elements, then retrieve
+all elements and place in the array from index 0 till set is empty, the last index where we inserted+1 will be the total 
+number of elements inserted(0 based indexing)so that many unique elements count .Rest of the array will be filled with 
+duplicates which we dont care anyway.
+T.C is : For insertion into set is O(N) for traversal of arr, each element insertion is O(1))
+if K unique elements ,we can get K by set.size then O(K) to insert set elements back to array 
+K can be upto N in worst case , so O(N)+O(N) overall O(N)
+S.C is O(K),worst case O(N) when all are Unique set needs N space.
+  // Function to remove duplicates from the array
+   function removeDuplicates(nums) {
+        // Set data structure to store unique elements
+        let s = new Set();
+        // Add all elements from array to the set
+        for (let val of nums) {
+            s.add(val);
+        }
+        // Get the number of unique elements
+        let k = s.size;
+        let j = 0;
+        // Copy unique elements from set to array
+        for (let val of s) {
+            nums[j++] = val;
+        }
+        // Return the number of unique elements
+        return k;
+    }
 */
 /*optimal: we keep track of count variable which tracks the count of placing the non duplicate value, its basically
 travelling the same array again and storing numbers like we always maintain non duplicate values till count index and 
