a37698eeface71b8e57705510cd0ab501654c5b7
anirudh sai <anirudh.sai.581@gmail.com>
2025-08-15 12:15:22 +0530

    folder change

---
+++ Arrays/Logic Building/06.IntersectionOfTwoSortedArrays.js
@@ -0,0 +1,11 @@
+/*
+Given two sorted arrays, nums1 and nums2, return an array containing the intersection of these two arrays. 
+Each element in the result must appear as many times as it appears in both arrays.
+The intersection of two arrays is an array where all values are present in both arrays.
+Input: nums1 = [1, 2, 2, 3, 5], nums2 = [1, 2, 7] Output: [1, 2]
+Input: nums1 = [1, 2, 2, 3, 3, 3], nums2 = [2, 3, 3, 4, 5, 7] Output: [2, 3, 3]
+Constraints:
+1 <= nums1.length, nums2.length <= 1000
+-104 <= nums1[i], nums2[i] <= 104
+Both nums1 and nums2 are sorted in non-decreasing order.
+*/
