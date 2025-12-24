18d5eb926baaccaa68ed4dcb371671da433ee34a
anirudh sai <anirudh.sai.581@gmail.com>
2025-12-24 23:42:47 +0530

    Pascals triangle 1,2 new solution info added

--- Arrays/FAQs(Medium)/04.PascalsTriangle1.js
+++ Arrays/FAQs(Medium)/04.PascalsTriangle1.js
@@ -32,7 +32,6 @@ c <= r
 All values will fit inside a 32-bit integer.
 */
 
-
 //1.Recusrive solution - Bad solution- T.C is 2**r, S.C is 2**r (callstack)
 /*
      Not valid solution ,it will work for very small r and c,(<10) etc 
@@ -49,7 +48,7 @@ All values will fit inside a 32-bit integer.
 //2.Generate Whole pascal triangle like DP, from start and there by check whatever index we want , generating pascal
 //triangle is O(n**2),space is O(n**2).
 //we can follow either 0 based indexing in our arrays or follow 1 based as pascal triangle in qn is 1-based .
- //1-based indexing solution:
+//1-based indexing solution:
 /*
     let triangle =[[]];
     for(let i=1;i<=r;i++){
@@ -69,8 +68,8 @@ All values will fit inside a 32-bit integer.
 //0-based indexing solution: pros: no need to store null in 0 index of row and triangle
 //just need to access at final given indexes -1 ,like triangle[r-1][c-1];
 //easy when printing whole table too
-   //this is brute force as we dont really require to have complete triangle .
-  //we have a direct formula based for finding value at a position in pascals triangle
+//this is brute force as we dont really require to have complete triangle .
+//we have a direct formula based for finding value at a position in pascals triangle
 /*
         let triangle =[];
         for(let i=0;i<r;i++){
@@ -87,30 +86,56 @@ All values will fit inside a 32-bit integer.
         }
         return  triangle[r-1][c-1];
 
+ 
+ OPTIMAL Logic (code below this section):        */
+
+/* Logic of ncr in general and for this problem, 
+  generally ncr means chose r items from n where order doesnt matter,it means a b c and b c a  and c a b etc are all equal 
+  as this is just selection. so whenever we are filling r slots from n items first one we can choose n, next n-1 , next n-2
+  like this for r items it is n*(n-1)*(n-2)*(n-3)*(n-4)*....(n-(r-1)), suppose we have to chose 2  from a,b,c,d,e we have 
+ab;ba;ac;ad;ae;ca;da;ea;bc;bd;be;cb;db;eb;cd;ce;dc;ec;de;ed total 20, since we know ab is same as ba in selection so we consider similar pairs as equal
+so we divide by 2! (r!) to avoid extra counting, like each r items can be of r! arrangements. so now we have (n*(n-1)*(n-2)*....(n-(r-1)))/r!) this is ncr
+in similarly in our pascal triangle, think of pascal triangle has a sequence of moves to reach any point from top, Each unique sequence of moves = one unique path.
+if we think of pascal triangle as grid coordinates :
+Row 1:        (1,1)
+
+Row 2:      (2,1) (2,2)
+
+Row 3:    (3,1) (3,2) (3,3)
+
+Row 4:  (4,1) (4,2) (4,3) (4,4)
+and allowed moves are down-left or down-right  similar to how we are generating each number as each number contributing to down-left and down-right
+To go from (1,1) yp (4,2) 
+Row 1 → Row 4:Total moves = 3
+To end at Column 2:You must move right exactly 1 time
+The rest must be left  So: Moves = L L R (in some order). we can have R L L , L R L so 3 unique paths, (4,2) will be 3.
+this is same as chosing 2 rights (column 2) in a possible path length of n(no.of rows above)   (ncr) is (3c2). which is 3.
+since in our problem this is 1 based indexing so we can do n-1 c r-1.also we know that nCr is same as nCn-r, so we can select min one 
+
+    
         */
 
-
-
-
 //Best Optimal:using nCr;
-  /*pascal triangle at r,c value is basically (r-1)C(c-1)(when 1-based indexing) so 
+/*pascal triangle at r,c value is basically (r-1)C(c-1)(when 1-based indexing) so 
   to calculate nCr instead of n! /r!*(n-r)! we can observe aftercancallelation its just 
   like in 7C2  => 7*6/2*1 also 7C2 is 7C5 so we if r>n-r we take r=n-r, then
     //time complexity is O(min(k,n-k));k will be column number O(C-1,r-c);
     //in worst case will be when c=r/2; in terms of big O, O(r);
   */
-function  pascalTriangleI(r, c) {
-
-    function nCr (n,k){ //optimal way of calulating nCr instead of computing factorial entirely.
-        if(k>n-k){k=n-k};
-        let res=1;
-        for(let i=0;i<k;i++){
-           res=res*(n-i); //for numerator like 7*6 
-           res=res/(i+1);//for denominator 1*2
-           //we can combine both these like res=res*(n-i)/(i+1); above for readabiity.
-        }
-        return res;
+function pascalTriangleI(r, c) {
+  function nCr(n, k) {
+    //optimal way of calulating nCr instead of computing factorial entirely.
+    if (k > n - k) {
+      k = n - k;
     }
-    return nCr(r-1,c-1);
- }
-console.log(pascalTriangleI(4,2));//3
+    let res = 1;
+    for (let i = 0; i < k; i++) {
+      res = res * (n - i); //for numerator like 7*6
+      res = res / (i + 1); //for denominator 1*2
+      //we can combine both these like res=res*(n-i)/(i+1); above for readabiity.
+    }
+    return res;
+  }
+  return nCr(r - 1, c - 1);
+}
+console.log(pascalTriangleI(4, 2)); //3
--- Arrays/FAQs(Medium)/05.PascalsTriangle2.js
+++ Arrays/FAQs(Medium)/05.PascalsTriangle2.js
@@ -28,7 +28,7 @@ All values will fit inside a 32-bit integer.
 */
 /*
 Brute:
-for given column r, we know there will be r elements , if we need to print row 4, we have 4 columns, also by formula we know 
+for given row r, we know there will be r elements , if we need to print row 4, we have 4 columns, also by formula we know 
 each will be r-1Cc-1, we can compute each element of row with this formula
 but the time complexity of finding nCk is O(k), if we had nth row then n elements
 so brute force with that formula is 
@@ -47,11 +47,31 @@ as the loop now runs for r times for given r its O(r).
 /earlier(direct formula for each element) its like O(r*r) as each element with formula takes O(r) in worst case.
 */
 function pascalTriangleII(r) {
-//c=r
-    let res=[1];//if r=6 ,6 columns(1 based indexing)
-    for(let c=2;c<=r;c++){
-        res[c-1]=res[c-2]*(r-(c-1))/(c-1);
-    }
-    return res;
+  //c=r
+  let res = [1]; //if r=6 ,6 columns(1 based indexing)
+  for (let c = 2; c <= r; c++) {
+    res[c - 1] = (res[c - 2] * (r - (c - 1))) / (c - 1);
+  }
+  return res;
 }
-console.log(pascalTriangleII(6));//[ 1, 5, 10, 10, 5, 1 ]
+console.log(pascalTriangleII(6)); //[ 1, 5, 10, 10, 5, 1 ]
+
+/* While loop version of the same pattern:
+     suppose for 5th row we can see 1 4 6 4 1 can be found using pattern: 
+     1*(4/1) then 4*(3/2) then 6*(2/3) then 4*(1/4) this way we ccan find for any row numerator will start with row number if 1 based indexing 
+     then r-1, and will go on till 1, then similarly denominator will start from 1 and go till row number, so r-1 (1based indexing)
+     ***Basically the logic is C(r-1, i) = C(r-1, i-1) × (r - i) / i
+     T.C is O(r), S.c is O(1)
+      pascalTriangleII(r) {
+        let ans=[1];
+        let num=r-1;let den=1;
+        let x=1;
+        while(num!=0){
+            x=x*num/den; // use x=x*num ; x/=den; if needed , but dont use x*=(num/den;as this will calculate num/den first
+            //  which will give float values
+            ans.push(x);
+            num--;den++;
+        }
+        return ans;
+    }
+*/
